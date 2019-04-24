22e814c76b59e26d7ef10e4f060932ce879745dd
Jacob Davison <c3324607@uon.edu.au>
2019-04-24 06:43:25 +0000

    trying

--- Decryption_Without_K.c
+++ Decryption_Without_K.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
-
-int main(void)
+int main()
 {
-    printf("hello, world\n");
+    char message[] = "";
+    int key;
+    int index;
+    int placeholder;
+    int indexKey;
+    for(index = 0; message[index] != 0; index++)
+    {
+        if(message[index] == 32)
+        {
+            int placeholder = message[index];
+            index = index + 2;
+            if(message[index] == 32)
+            {
+                index = index - 1;
+                message[indexKey] = message[index];
+                message[index] = 73;
+                key = message[indexKey] - 73;
+                goto Decrypt;
+            }
+        }
+    }
+    Decrypt:
+    {
+        for(index = 0; message[index] != 0; index++)
+        {
+            message[index] =  message[index] - key;
+        }
+    }
+    printf("Message is:", message);
+    return 0;
 }
+
